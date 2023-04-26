85f00974b8c67ca57e80d74776eda90a416cd3e1
nopphawong.n <nopphawong.n@gmail.com>
2023-04-26 13:43:07 +0700

    commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+/**
+ * @desc Login
+ * @route POST /auth
+ * @access public
+ */
+const login = async (req, res) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'All fields are required' })
+  }
+
+  const userFound = await User.findOne({ username }).exec()
+
+  if (!userFound || !userFound.active) {
+    return res.status(401).json({ success: false, message: 'Unauthorized' })
+  }
+
+  const match = await bcrypt.compare(password, userFound.password)
+
+  if (!match)
+    return res.status(401).json({ success: false, message: 'Unauthorized' })
+}
+
+/**
+ * Logout
+ * @route POST /auth/logout
+ * @access public
+ */
