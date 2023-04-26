21453862354f131ca368b20c30c30c581f28e188
nopphawong.n <nopphawong.n@gmail.com>
2023-04-26 14:33:11 +0700

    commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,26 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
+
+const getUserID = async () => {
+    const userFound = await User.findOne()
+      .sort({ _id: -1 })
+      .limit(1)
+      .lean()
+      .exec()
+    if (!userFound) {
+      return 'U0000'
+    } else {
+      let userID = userFound.user_id
+      if (userID.substr(0, 1) === 'U') {
+        userID = userID.substr(1)
+        const numberUser = parseInt(userID, 10)
+        userID = ('0000' + (numberUser + 1)).slice(-4)
+        return `U${userID}`
+      } else {
+        return ''
+      }
+    }
+  }
 
 /**
  * Create new User
@@ -7,7 +29,7 @@ const User = require('../models/User')
  */
 const createNewUser = async (req, res) => {
   try {
-    const { username, password, name } = req.body
+    const { username, password, name, role } = req.body
 
     if (!username || !password || !name) {
       return res
@@ -23,6 +45,20 @@ const createNewUser = async (req, res) => {
         .status(409)
         .json({ success: false, message: 'Duplicate Username' })
     }
+
+    // NOTE: Hash password
+    const hashedPwd = bcrypt.hashSync(password, 10)
+    const userID = await getUserID()
+    const userObject = {
+        user_id: userID,
+        username,
+        password: hashedPwd,
+        name,
+        role
+    }
+
+    const user = await User.create(userObject)
+
   } catch (err) {
     res.status(400).json({ success: false, message: err })
   }
