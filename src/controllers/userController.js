463d5e4d2433dc9380c472dc2886c6c334d2773b
nopphawong.n <nopphawong.n@gmail.com>
2023-04-26 14:23:19 +0700

    commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 /**
  * Create new User
  * @route POST /users
@@ -6,10 +8,20 @@
 const createNewUser = async (req, res) => {
   try {
     const { username, password, name } = req.body
+
     if (!username || !password || !name) {
       return res
         .status(400)
         .json({ success: false, message: 'All fields are required' })
+    }
+
+    // NOTE: Check duplicate.
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate) {
+      return res
+        .status(409)
+        .json({ success: false, message: 'Duplicate Username' })
     }
   } catch (err) {
     res.status(400).json({ success: false, message: err })
